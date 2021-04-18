c819b762ebc8103f1eb47590165fb2fe4d7514a8
Amir Afshar <mr.afshar0@gmail.com>
2021-04-18 17:31:30 +0430

    Change src of banners on mobile/desktop

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
+"use strict";
 const burger = document.querySelector(".hamburger");
 const closeburger = document.querySelector(".close-burger");
 const nav = document.querySelector(".navmenu");
 const interactiveImg = document.querySelector(".interactive");
-
+const creations = document.querySelectorAll(".our-creations");
 
 // Hamburger Menu
 
-burger.addEventListener("click", function(){
-    nav.classList.add("open");
+burger.addEventListener("click", function () {
+  nav.classList.add("open");
 });
-closeburger.addEventListener("click", function(){
-    nav.classList.remove("open");
-})
-
-
+closeburger.addEventListener("click", function () {
+  nav.classList.remove("open");
+});
 
 // Change the interactive image source on desktop/mobile design
 
-const imgChange = function(){
-    if(window.innerWidth > 654){
-        interactiveImg.src = "images/desktop/image-interactive.jpg";
-    }else{
-        interactiveImg.src = "images/mobile/image-interactive.jpg";
-    }
-}();
+const interactiveSrcChange = (function () {
+  if (window.innerWidth > 654) {
+    interactiveImg.src = "images/desktop/image-interactive.jpg";
+  } else {
+    interactiveImg.src = "images/mobile/image-interactive.jpg";
+  }
+})();
+
+// Change Our Creations Banner src on desktop/mobile design
+
+const bannerSrcChange = (function () {
+  if (window.innerWidth > 654) {
+    creations.forEach((creation) => {
+      creation.src = creation.src.replace("mobile", "desktop");
+    });
+  } else {
+    creations.forEach((creation) => {
+      creation.src = creation.src.replace("desktop", "mobile");
+    });
+  }
+})();
