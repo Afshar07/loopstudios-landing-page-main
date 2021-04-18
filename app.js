22385840cbe51a67fceeec7465c62bf592b72435
Amir Afshar <mr.afshar0@gmail.com>
2021-04-18 14:55:37 +0430

    Changing the interactive picture for mobile/desktop

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const burger = document.querySelector(".hamburger");
 const closeburger = document.querySelector(".close-burger");
 const nav = document.querySelector(".navmenu");
+const interactiveImg = document.querySelector(".interactive");
+
+
+// Hamburger Menu
 
 burger.addEventListener("click", function(){
     nav.classList.add("open");
@@ -8,3 +12,15 @@ burger.addEventListener("click", function(){
 closeburger.addEventListener("click", function(){
     nav.classList.remove("open");
 })
+
+
+
+// Change the interactive image source on desktop/mobile design
+
+const imgChange = function(){
+    if(window.innerWidth > 654){
+        interactiveImg.src = "images/desktop/image-interactive.jpg";
+    }else{
+        interactiveImg.src = "images/mobile/image-interactive.jpg";
+    }
+}();
